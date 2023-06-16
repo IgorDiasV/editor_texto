47dfccd7e94feb4e406dcd3899c34a8ae451ebbf
Igor Dias <igorverissimo02@hotmail.com>
2023-06-16 11:05:08 -0300

    criação de um arquivo com as configurações do quill

---
+++ definicoes_quill.js
@@ -0,0 +1,29 @@
+var toolbarOptions = [
+    ["bold", "italic", "underline", "strike"],
+    ["blockquote", "code-block"],
+
+    [{ header: 1 }, { header: 2 }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ indent: "-1" }, { indent: "+1" }],
+    [{ direction: "rtl" }],
+
+    [{ size: ["small", false, "large", "huge"] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }],
+    [{ font: [] }],
+    [{ align: [] }],
+    ["image"],
+    ["clean"],
+  ];
+  let definicao_quill = {
+    theme: "snow",
+    modules: {
+      imageResize: {},
+      toolbar: {
+        container: toolbarOptions,
+      },
+    },
+    placeholder: "Digite a questão",
+  };
