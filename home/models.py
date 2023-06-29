8ea870a7570cc8855f089d2d476d2132d1e37800
Igor Dias <igorverissimo02@hotmail.com>
2023-06-29 08:38:18 -0300

    feat: Adicionado a opção de salvar os textos em um banco de dados

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Texto(models.Model):
+    id = models.AutoField(primary_key=True)
+    texto = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Texto
+import json
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def salvar(request):
+    dados = json.loads(request.body)
+    texto = dados['texto']
+
+    Texto.objects.create(texto=texto)
+    resposta = {'mensagem':'Texto salvo com Sucesso'}
+    resposta = json.dumps(resposta)
+    return HttpResponse(resposta)
