19a81cedaadc0095be69460d00083dd6c73eee03
Igor Dias <igorverissimo02@hotmail.com>
2023-07-27 14:13:02 -0300

    refactor: replacing fetch with a form

--- home/views.py
+++ home/views.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Texto
-import json
-# Create your views here.
+
+
 def home(request):
     return render(request, 'index.html')
 
-def salvar(request):
-    dados = json.loads(request.body)
-    texto = dados['texto']
 
+def salvar(request):
+    texto = request.POST['texto']
     Texto.objects.create(texto=texto)
-    resposta = {'mensagem':'Texto salvo com Sucesso'}
-    resposta = json.dumps(resposta)
-    return HttpResponse(resposta)
+    return redirect('home')
+
 
 def textos_salvos(request):
     textos = Texto.objects.all()
-    return render(request,'textos_salvos.html', {'textos': textos})
+    return render(request, 'textos_salvos.html', {'textos': textos})
